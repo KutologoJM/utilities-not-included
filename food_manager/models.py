ea2654163f5de95a5515a7b0388e4b9ba4daec67
KutologoJM <kutologomashego@gmail.com>
2026-01-12 22:28:53 +0800

    Reduced SQL query load times by pre-fetching related data

--- food_manager/models.py
+++ food_manager/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django_extensions.db.fields import AutoSlugField
+from django.db.models import Prefetch
 
 
 # Create your models here.
+
 
 class FoodQuality(models.Model):
     quality = models.CharField(max_length=10, unique=True)
@@ -27,7 +29,7 @@ class Recipe(models.Model):
         UNITS = "units", "Units"
         GRAMS = "g", "Grams"
 
-    name = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=50, unique=True, db_index=True)
     wiki_url = models.URLField(blank=True)  # null = True not recommended
     image_url = models.URLField(blank=True)
     description = models.TextField(blank=True)
@@ -38,7 +40,7 @@ class Recipe(models.Model):
     kcal_per_kg = models.PositiveIntegerField(null=True, blank=True)
     food_gained = models.CharField(max_length=100, blank=True)
 
-    slug = AutoSlugField(unique=True, populate_from='name')
+    slug = AutoSlugField(unique=True, populate_from='name', db_index=True)
 
     sources = models.ForeignKey("FoodItemSource", on_delete=models.PROTECT)
     food_quality = models.ForeignKey("FoodQuality", on_delete=models.PROTECT, null=True)
@@ -49,15 +51,15 @@ class Recipe(models.Model):
 
     @property
     def required_ingredients(self):
-        return self.recipe_ingredients.filter(
-            role=RecipeIngredient.Roles.REQUIRED
-        )
+        # Use the prefetched list if available, fallback to DB query
+        return getattr(self, 'prefetched_required_ingredients',
+                       self.recipe_ingredients.filter(role=RecipeIngredient.Roles.REQUIRED))
 
     @property
     def substitutable_ingredients(self):
-        return self.recipe_ingredients.filter(
-            role=RecipeIngredient.Roles.SUBSTITUTABLE
-        )
+        # Use the prefetched list if available, fallback to DB query
+        return getattr(self, 'prefetched_required_ingredients',
+                       self.recipe_ingredients.filter(role=RecipeIngredient.Roles.SUBSTITUTABLE))
 
     class Meta:
         verbose_name = "Recipe"
@@ -112,3 +114,37 @@ class RecipeIngredient(models.Model):
 
     def __str__(self):
         return f"{self.amount} {self.unit} of {self.ingredient.name} for {self.recipe.name}"
+
+
+"""
+class Blueprint(models.Model):
+    class Goals(models.TextChoices):
+        support_x_dupes = "supp_x_dupes", "Support x dupes"
+        last_x_cycles = "last_x_cycles", "Last x cycles"
+    blueprint_name = models.CharField(max_length=50, unique=True)
+    colony_name = models.CharField(max_length=50)
+    num_of_duplicants = models.PositiveIntegerField(default=0)
+    hunger_setting = models.ForeignKey("HungerLevels", on_delete=models.PROTECT)
+    current_goal = models.CharField(choices=Goals, default=Goals.last_x_cycles, max_length=20)
+    chosen_foods = models.TextField(blank=True) # todo change to a m2m field with a through table for amounts
+
+    @property
+    def daily_kcal_needed(self):
+        # for ingredient in chosen ingredients, num dupes x difficulty
+        return 0
+
+    @property
+    def kcal_produced(self):
+        # for ingredient in chosen ingredients, ingredient.you get x num of chosen ingredient, ?move to through table
+        return 0
+
+    @property
+    def num_of_survivable_cycles(self):
+        # num of " cycles = food produced / num of dupes * hunger difficulty
+        return 0
+
+
+class HungerLevels(models.Model):
+    level = models.CharField(max_length=50)
+    kcal_required = models.PositiveIntegerField(default=1000)
+"""
--- food_manager/views.py
+++ food_manager/views.py
@@ -1,22 +1,72 @@
 from django.shortcuts import render
-from food_manager.models import Recipe
-from django.db.models import Q
+from food_manager.models import Recipe, RecipeIngredient
+from django.db.models import Q, Prefetch
+
 
 # Create your views here.
 
+def prefetch_all_recipes():
+    data = Recipe.objects.all().prefetch_related(
+        Prefetch(
+            'recipe_ingredients',
+            queryset=RecipeIngredient.objects.filter(role='required').select_related('ingredient'),
+            to_attr='prefetched_required_ingredients'
+        ),
+        Prefetch(
+            'recipe_ingredients',
+            queryset=RecipeIngredient.objects.filter(role='subst').select_related('ingredient'),
+            to_attr='prefetched_subst_ingredients'
+        ),
+        'sources',
+        'dlc',
+        'ingredients',
+        'food_quality',
+        'recipe_ingredients',
+        'recipe_ingredients__ingredient',
+        'recipe_ingredients__ingredient__sources',
+        'recipe_ingredients__ingredient__dlc',
+        'recipe_ingredients__ingredient__food_quality',
+    )
+    return data
+
+
 def index(request):
     context = {}
-    recipes = Recipe.objects.all().order_by('-id')
+    recipes = prefetch_all_recipes()
     context['recipes'] = recipes
     return render(request, 'foods/recipes.html', context)
 
-def search_recipes(request):
-    query = request.GET.get('search', '')
-    order_by = request.GET.get('order_by', 'name')
 
-    # use the query to filter recipes by name or slug
-    recipes = Recipe.objects.filter(
-        Q(name__icontains=query) | Q(slug__icontains=query)
-    )
-    recipes.order_by(order_by)
-    return render(request, "partials/recipe-list.html", {'recipes': recipes})
+def search_recipes(request):
+    context = {}
+    query = request.GET.get('search', '')
+    if query == '' or query is None:
+        recipes = prefetch_all_recipes()
+        context['recipes'] = recipes
+    else:
+        # use the query to filter recipes by name or slug
+        recipes = Recipe.objects.filter(
+            Q(name__icontains=query) | Q(slug__icontains=query)
+        ).prefetch_related(
+            Prefetch(
+                'recipe_ingredients',
+                queryset=RecipeIngredient.objects.filter(role='required').select_related('ingredient'),
+                to_attr='prefetched_required_ingredients'
+            ),
+            Prefetch(
+                'recipe_ingredients',
+                queryset=RecipeIngredient.objects.filter(role='subst').select_related('ingredient'),
+                to_attr='prefetched_subst_ingredients'
+            ),
+            'sources',
+            'dlc',
+            'ingredients',
+            'food_quality',
+            'recipe_ingredients',
+            'recipe_ingredients__ingredient',
+            'recipe_ingredients__ingredient__sources',
+            'recipe_ingredients__ingredient__dlc',
+            'recipe_ingredients__ingredient__food_quality',
+        )
+        context['recipes'] = recipes
+    return render(request, "partials/recipe-list.html", context=context)
